b5e55a35abaa8e92e113f36ab092fde69a761ae5
Juan Jimeno <jimenojmm@gmail.com>
2017-06-16 10:57:00 +0800

    added script to change executable to package name

--- gencodes.sh
+++ gencodes.sh
@@ -6,6 +6,7 @@ rm include/*.h
 rm src/*.cxx
 rm src/make*
 
+PACKAGE_NAME=$(basename $PWD)
 #get the new idl file created
 CURRENT_IDL=$(basename ./idl/*.idl .idl)
 #get prev idl file 
@@ -18,7 +19,10 @@ rm make*
 
 cd ..
 
-# #change the filenames on CMakeLists based on newly generated codes
+# change the main app's filename same as package name
+perl -pi -e "s/fastrtps_ros/$PACKAGE_NAME/g" CMakeLists.txt
+
+#change the filenames on CMakeLists based on newly generated codes
 perl -pi -e "s/$PREV_IDL/$CURRENT_IDL/g" CMakeLists.txt
 
 #store the previous idl name so the bash file knows what to replace next call
